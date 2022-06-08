86e9e9fce0f699a69467923e7836e31270ca0877
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2022-06-08 19:35:21 +0700

    feat: test reverse word

---
+++ interview/reverse_word_test.go
@@ -0,0 +1,43 @@
+package interview
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverseWord(t *testing.T) {
+	// Create a function that receive input and will give output:
+
+	// Reverse each word of these:
+
+	// "I am A Great human"
+	// into
+	// "I ma A Taerg namuh"
+
+	// note: observe the Capital word behavior. Capital only when in the first letter
+
+	assert.Equal(t, reverseWord("I am A Great human"), "I ma A Taerg namuh", `The word "I am A Great human" is not pass`)
+}
+
+func reverseWord(str string) string {
+	reverse := func(str string) string {
+		var res string
+		for _, v := range str {
+			res = string(v) + res
+		}
+		return res
+	}
+
+	arrStr := strings.Split(str, " ")
+	for idx, val := range arrStr {
+		strReverse := reverse(val)
+		if val == strings.Title(val) {
+			arrStr[idx] = strings.Title(strings.ToLower(strReverse))
+			continue
+		}
+		arrStr[idx] = strReverse
+	}
+	return strings.Join(arrStr, " ")
+}
